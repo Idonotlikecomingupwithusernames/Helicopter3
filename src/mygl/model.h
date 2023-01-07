094fe5b02e7f4f9bfd972dba013be77548f627f6
Idonotlikecomingupwithusernames <Idonotlikecomingupwithusernames@users.noreply.github.com>
2023-01-07 17:02:01 +0100

    Initial Commit + .gitignore

---
+++ src/mygl/model.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "mesh.h"
+#include "texture.h"
+
+struct Material
+{
+    std::string name;
+
+    Vector3D emission;
+    Vector3D ambient;
+    Vector3D diffuse;
+    Vector3D specular;
+    float shininess;
+
+    Texture map_diffuse;
+    Texture map_specular;
+    Texture map_emission;
+    Texture map_ambient_occlusion;
+
+    unsigned int indexOffset;
+    unsigned int indexCount;
+};
+
+struct Model
+{
+    Mesh mesh;
+    std::string name;
+    std::vector<Material> material;
+};
+
+std::vector<Model> modelLoad(const std::string &filepath);
+void modelDelete(std::vector<Model>& models);
+void modelDelete(Model& model);
