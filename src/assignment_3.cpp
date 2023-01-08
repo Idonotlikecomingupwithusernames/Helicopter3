f0614682845c16089745a078bc95afc54a9cdef6
Idonotlikecomingupwithusernames <Idonotlikecomingupwithusernames@users.noreply.github.com>
2023-01-08 10:24:37 +0100

    Imported Changes from last assignment
    Front-facing spotlight has gone missing.

--- src/assignment_3.cpp
+++ src/assignment_3.cpp
@@ -7,6 +7,18 @@
 
 #include "helicopter.h"
 
+struct SpotLight
+{
+    Vector3D position;
+    Vector3D direction;
+    Vector4D color;
+
+    float constant;
+    float linear;
+    float quadratic; 
+    float phi;
+};
+
 struct
 {
     Camera camera;
@@ -17,6 +29,13 @@ struct
     Model modelGround;
 
     ShaderProgram shaderColor;
+
+    SpotLight whiteLights[3];
+    SpotLight redLight;
+
+    bool whiteLightsAreOn = true;
+    bool redLightIsOn = true;
+    bool isDay = false;
 } sScene;
 
 struct
@@ -94,6 +113,16 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
     {
         screenshotToPNG("screenshot.png");
     }
+
+    /* Switch between day/night */
+    if(key == GLFW_KEY_M && action == GLFW_PRESS){
+        sScene.isDay = !sScene.isDay;
+    }
+
+    /* Switch white lights on/off */
+    if(key == GLFW_KEY_N && action == GLFW_PRESS){
+        sScene.whiteLightsAreOn = !sScene.whiteLightsAreOn;
+    }
 }
 
 void mousePosCallback(GLFWwindow* window, double x, double y)
@@ -141,6 +170,40 @@ void sceneInit(float width, float height)
     sScene.modelGround = modelLoad("assets/ground/ground.obj").front();
 
     sScene.shaderColor = shaderLoad("shader/default.vert", "shader/color.frag");
+
+    /* Setup lights very inefficiently */
+    sScene.whiteLights[0].position = Vector3D(-1.0, 5.0, -2.0);
+    sScene.whiteLights[1].position = Vector3D(1.0, 5.0, -2.0);
+    sScene.whiteLights[0].direction = Vector4D(1.0, 0.0, 0.0, 1.0);
+    sScene.whiteLights[1].direction = Vector4D(-1.0, 0.0, 0.0, 1.0);
+
+    for(int i = 0; i < 2; i++){
+        sScene.whiteLights[i].color = Vector4D(1.0, 1.0, 1.0, 1.0);
+        sScene.whiteLights[i].constant = 0.4;
+        sScene.whiteLights[i].linear = 0.09;
+        sScene.whiteLights[i].quadratic = 0.032;
+        sScene.whiteLights[i].phi = to_radians(30);
+    }
+
+    sScene.whiteLights[2] = {
+        Vector3D(0.0, 5.0, 0.0),
+        Vector3D(0.0, 0.0, 1.0),
+        Vector4D(1.0, 1.0, 1.0, 1.0),
+        0.6,
+        0.09,
+        0.032,
+        to_radians(50.0)
+    };
+
+    sScene.redLight = {
+        Vector3D(0.0, 2.0, -5.0),
+        Vector3D(0.0, 1.0, 0.0),
+        Vector4D(1.0, 0.2, 0.2, 1.0),
+        0.6,
+        0.09,
+        0.032,
+        to_radians(-60.0)
+    };
 }
 
 void sceneUpdate(float dt)
@@ -149,6 +212,9 @@ void sceneUpdate(float dt)
 
     if (sScene.cameraFollowHeli)
         cameraFollow(sScene.camera, sScene.heli.position);
+
+    /* Toggle strobe every 2 seconds (takes a few seconds to work properly) */
+    sScene.redLightIsOn = !int(fmod(int(glfwGetTime()), 2)) ? !sScene.redLightIsOn : sScene.redLightIsOn;
 }
 
 void sceneDraw()
@@ -167,6 +233,68 @@ void sceneDraw()
         shaderUniform(sScene.shaderColor, "uView",  view);
         shaderUniform(sScene.shaderColor, "uModel",  sScene.heli.transformation);
 
+        /* Pass camera position to calculate viewDir */
+        shaderUniform(sScene.shaderColor, "uCamPos",  sScene.camera.position);
+
+        /* Pass ambient factor */
+        shaderUniform(sScene.shaderColor, "uAmbient",  float(0.1));
+
+        /* Pass directional light */
+        shaderUniform(sScene.shaderColor, "uDirLight.direction",  Vector3D(0.0, -1.0, -1.0));
+
+        if (sScene.isDay){
+            shaderUniform(sScene.shaderColor, "uDirLight.color",  Vector4D(0.4, 0.4, 0.3, 1.0));
+        } else {
+            shaderUniform(sScene.shaderColor, "uDirLight.color",  Vector4D(0.05, 0.05, 0.07, 1.0));
+        }
+
+        /* Toggle white lights */
+        if (sScene.whiteLightsAreOn){
+            shaderUniform(sScene.shaderColor, "uSpotLight[0].color", sScene.whiteLights[0].color);
+            shaderUniform(sScene.shaderColor, "uSpotLight[1].color", sScene.whiteLights[1].color);
+            shaderUniform(sScene.shaderColor, "uSpotLight[2].color", sScene.whiteLights[2].color);
+        } else {
+            shaderUniform(sScene.shaderColor, "uSpotLight[0].color",  Vector4D(0.0, 0.0, 0.0, 1.0));
+            shaderUniform(sScene.shaderColor, "uSpotLight[1].color",  Vector4D(0.0, 0.0, 0.0, 1.0));
+            shaderUniform(sScene.shaderColor, "uSpotLight[2].color",  Vector4D(0.0, 0.0, 0.0, 1.0));
+        }
+
+        /* Toggle strobe */
+        if (sScene.redLightIsOn){
+            shaderUniform(sScene.shaderColor, "uSpotLight[3].color", sScene.redLight.color);
+        } else {
+            shaderUniform(sScene.shaderColor, "uSpotLight[3].color",  Vector4D(0.0, 0.0, 0.0, 1.0));
+        }
+
+        /* Pass light values very inefficiently */
+        shaderUniform(sScene.shaderColor, "uSpotLight[0].position", sScene.whiteLights[0].position);
+        shaderUniform(sScene.shaderColor, "uSpotLight[0].direction", sScene.whiteLights[0].direction);
+        shaderUniform(sScene.shaderColor, "uSpotLight[0].constant", sScene.whiteLights[0].constant);
+        shaderUniform(sScene.shaderColor, "uSpotLight[0].linear", sScene.whiteLights[0].linear);
+        shaderUniform(sScene.shaderColor, "uSpotLight[0].quadratic", sScene.whiteLights[0].quadratic);
+        shaderUniform(sScene.shaderColor, "uSpotLight[0].phi", sScene.whiteLights[0].phi);
+
+        shaderUniform(sScene.shaderColor, "uSpotLight[1].position", sScene.whiteLights[1].position);
+        shaderUniform(sScene.shaderColor, "uSpotLight[1].direction", sScene.whiteLights[1].direction);
+        shaderUniform(sScene.shaderColor, "uSpotLight[1].constant", sScene.whiteLights[1].constant);
+        shaderUniform(sScene.shaderColor, "uSpotLight[1].linear", sScene.whiteLights[1].linear);
+        shaderUniform(sScene.shaderColor, "uSpotLight[1].quadratic", sScene.whiteLights[1].quadratic);
+        shaderUniform(sScene.shaderColor, "uSpotLight[1].phi", sScene.whiteLights[1].phi);
+
+        shaderUniform(sScene.shaderColor, "uSpotLight[2].position", sScene.whiteLights[2].position);
+        shaderUniform(sScene.shaderColor, "uSpotLight[2].direction", sScene.whiteLights[2].direction);
+        shaderUniform(sScene.shaderColor, "uSpotLight[2].constant", sScene.whiteLights[2].constant);
+        shaderUniform(sScene.shaderColor, "uSpotLight[2].linear", sScene.whiteLights[2].linear);
+        shaderUniform(sScene.shaderColor, "uSpotLight[2].quadratic", sScene.whiteLights[2].quadratic);
+        shaderUniform(sScene.shaderColor, "uSpotLight[2].phi", sScene.whiteLights[2].phi);
+
+        shaderUniform(sScene.shaderColor, "uSpotLight[3].position", sScene.redLight.position);
+        shaderUniform(sScene.shaderColor, "uSpotLight[3].direction", sScene.redLight.direction);
+        shaderUniform(sScene.shaderColor, "uSpotLight[3].constant", sScene.redLight.constant);
+        shaderUniform(sScene.shaderColor, "uSpotLight[3].linear", sScene.redLight.linear);
+        shaderUniform(sScene.shaderColor, "uSpotLight[3].quadratic", sScene.redLight.quadratic);
+        shaderUniform(sScene.shaderColor, "uSpotLight[3].phi", sScene.redLight.phi);
+
         /* render heli */
         for(unsigned int i = 0; i < sScene.heli.partModel.size(); i++)
         {
@@ -180,6 +308,13 @@ void sceneDraw()
             {
                 /* set material properties */
                 shaderUniform(sScene.shaderColor, "uMaterial.diffuse", material.diffuse);
+                /*
+                * This is the correct implementation, which unfortunetaly leads to whitewashing everything
+                shaderUniform(sScene.shaderColor, "uMaterial.ambient", Vector4D(material.ambient, 1.0));
+                */
+                shaderUniform(sScene.shaderColor, "uMaterial.diffuse", Vector4D(material.diffuse, 1.0));
+                shaderUniform(sScene.shaderColor, "uMaterial.specular", Vector4D(material.specular, 1.0));
+                shaderUniform(sScene.shaderColor, "uMaterial.shininess", material.shininess);
 
                 glDrawElements(GL_TRIANGLES, material.indexCount, GL_UNSIGNED_INT, (const void*) (material.indexOffset*sizeof(unsigned int)) );
             }
@@ -193,6 +328,13 @@ void sceneDraw()
         {
             /* set material properties */
             shaderUniform(sScene.shaderColor, "uMaterial.diffuse", material.diffuse);
+            /*
+            * This is the correct implementation, which unfortunetaly leads to whitewashing everything
+            shaderUniform(sScene.shaderColor, "uMaterial.ambient", Vector4D(material.ambient, 1.0));
+            */
+            shaderUniform(sScene.shaderColor, "uMaterial.diffuse", Vector4D(material.diffuse, 1.0));
+            shaderUniform(sScene.shaderColor, "uMaterial.specular", Vector4D(material.specular, 1.0));
+            shaderUniform(sScene.shaderColor, "uMaterial.shininess", material.shininess);
 
             glDrawElements(GL_TRIANGLES, material.indexCount, GL_UNSIGNED_INT, (const void*) (material.indexOffset*sizeof(unsigned int)) );
         }
--- src/shader/color.frag
+++ src/shader/color.frag
@@ -1,15 +1,102 @@
 #version 330 core
 
-struct Material
-{
-    vec3 diffuse;
-};
-
 out vec4 FragColor;
 
+in vec3 tNormal;
+in vec3 tFragPos;
+in vec2 tUV;
+
+struct Material
+{
+    vec4 ambient;
+    vec4 diffuse;
+    vec4 specular;
+    float shininess;
+};
+
+struct DirLight 
+{
+    vec3 direction;
+    vec4 color;
+};
+
+struct SpotLight 
+{
+    vec3 position;
+    vec3 direction;
+    vec4 color;
+
+    float constant;
+    float linear;
+    float quadratic; 
+    float phi;
+};
+
 uniform Material uMaterial;
+uniform vec3 uCamPos;
+
+uniform DirLight uDirLight;
+uniform SpotLight uSpotLight[4];
+uniform float uAmbient;
+
+vec3 bPDir(vec3 lightColor, vec3 lightDir, vec3 viewDir, vec3 normal, Material material) {
+    /* Calculate ambient */
+    vec3 ambient = lightColor * material.ambient.rgb;
+    /* Calculate diffuse */
+    vec3 diffuse = lightColor * (max(dot(normal, lightDir), 0.0) * material.diffuse.rgb);
+    /* Calculate specular */
+    vec3 halfDir = normalize(viewDir + lightDir);
+    float specDot = max(dot(halfDir, normal), 0.0);
+    vec3 specular = lightColor * pow(specDot, material.shininess) * material.specular.rgb;
+
+    return ambient + diffuse + specular;
+}
+
+vec3 bPSpot(SpotLight spotLight, vec3 viewDir, vec3 normal, vec3 fragPos, Material material) {
+    vec3 lightDir = normalize(spotLight.position - fragPos);
+    float theta = dot(lightDir, normalize(-spotLight.direction));
+
+    vec3 lightSum = vec3(0.0, 0.0, 0.0);
+
+    if(theta > spotLight.phi) 
+    {
+        /* Calculate ambient */
+        vec3 ambient = uAmbient * spotLight.color.rgb * material.ambient.rgb;
+        /* Calculate diffuse */
+        vec3 diffuse = spotLight.color.rgb * (max(dot(normal, lightDir), 0.0) * material.diffuse.rgb);
+        /* Calculate specular */
+        vec3 halfDir = normalize(viewDir + lightDir);
+        float specDot = max(dot(halfDir, normal), 0.0);
+        vec3 specular = spotLight.color.rgb * pow(specDot, material.shininess) * material.specular.rgb;
+        /* Calculate attenuation */
+        float distance = length(spotLight.position - fragPos);
+        float attenuation = 1.0 / (spotLight.constant + spotLight.linear * distance + spotLight.quadratic * (distance * distance));
+
+        lightSum += (ambient + diffuse + specular) * attenuation;
+    }
+    
+
+    return lightSum;
+} 
 
 void main(void)
 {
-    FragColor = vec4(uMaterial.diffuse, 1.0);
+    vec3 lightSum = vec3(0.0);
+    vec3 ptlts = vec3(0.0);
+
+    vec3 viewDir = normalize(uCamPos - tFragPos);
+    vec3 n = normalize(tNormal);
+
+    /* Calculate directional light */
+    vec3 dLightDir = normalize(-uDirLight.direction);
+
+    lightSum += bPDir(uDirLight.color.rgb, dLightDir, viewDir, n, uMaterial);
+
+    /* Calculate spotlights */
+    for(int i = 0; i < 4; i++){
+        ptlts += bPSpot(uSpotLight[i], viewDir, n, tFragPos, uMaterial);
+    }
+
+    lightSum += ptlts;
+    FragColor = vec4(lightSum, 1.0);
 }
